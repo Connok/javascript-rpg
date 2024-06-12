dcd1ed04847c16e9cd1e74c944d188f6f7e618ab
Connor McCarthy <mccarthyrcconnor@gmail.com>
2024-06-12 10:41:34 -0400

    Game Complete

--- script.js
+++ script.js
@@ -76,11 +76,30 @@ const locations = [
     },
     {
         name: "fight",
-        "button text": ["Attack", "Doge", "Run"],
+        "button text": ["attack", "doge", "run"],
         "button functions": [attack, doge, goTown],
         text: "You are fighting a monster."
 
+    },
+    {
+        name: "killMonster",
+        "button text": ["Go To Town Square", "Go To Town Square", "Go To Town Square"],
+        "button functions": [goTown, goTown, goTown],
+        text: `The monster Screems "Arg!" as it dies. You gain experiance points and find gold.`
+    },
+    {
+        name: "lose",
+        "button text": ["Replay?", "Replay?", "Replay?"],
+        "button functions": [restart, restart, restart],
+        text: `You Died.`
+    },
+    {
+        name: "win",
+        "button text": ["Replay?", "Replay?", "Replay?"],
+        "button functions": [restart, restart, restart],
+        text: `You Defeat the Dragon! YOU WIN THE GAME!`
     }
+
 
 ];
 
@@ -92,6 +111,7 @@ button3.onclick = fightDragon;
 
 
 function update(location){
+    monsterStats.style.display = "none";
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
@@ -179,11 +199,44 @@ function attack(){
     text.innerText = "The " + monsters[fighting].name + " attacks";
     text.innerText = "You attackit with your  " + weapons[currentWeapon].name + ".";
     health -= monsters[fighting].level;
-    monsterHealth -= weapons[currentWeapon].power + Math.floor(random() * xp) + 1;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
     text.innerText = ""
 
+    if( health <= 0 ){
+        lose();
+    }else if (monsterHealth <=0) {
+        fighting === 2 ? winGame() : defeteMonster();
+    }
+
 
 };
-function doge(){};
+function doge(){
+    text.innerText = "You Doged the attack from " + monsters[fighting].name + ".";
+};
+function defeteMonster(){
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+
+    update(locations[4])
+};
+function winGame(){
+    update(locations[6]);
+};
+function lose(){
+    update(locations[5])
+};
+function restart(){
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ["stick"];
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+    goTown();
+};
